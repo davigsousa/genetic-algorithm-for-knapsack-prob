62cf7667273991b93efa6dd3d0f069da42c917ac
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2023-04-16 10:37:53 -0300

    fix: update params according with description

--- types/problem.ts
+++ types/problem.ts
@@ -1,12 +1,10 @@
 export interface Bag {
   height: number;
   width: number;
-  depth: number;
 }
 
 export interface Item {
   height: number;
   width: number;
-  depth: number;
   priceValue: number;
 }
