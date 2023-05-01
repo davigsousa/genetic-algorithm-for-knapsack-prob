09ee30d844f461ee0189437758bf8c6eba883ca9
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2023-05-01 01:31:37 -0300

    fix: update problem description

--- components/problem-description.tsx
+++ components/problem-description.tsx
@@ -10,11 +10,11 @@ export default function ProblemDescription() {
       textAlign="center"
     >
       <Typography variant="body2">
-        In this version of the knapsack problem, a Genetic Algorithm will
-        attempt to find the best combination of boxes to put in a warehouse, in
+        A Genetic Algorithm will attempt to find the best combination (which
+        produces the higher price amount) of boxes to put in a warehouse, in
         which you need to specify the warehouse area, weight limit and the
-        quantity of random boxes. For simplicity, assume both the warehouse and
-        the boxes are rectangles.
+        quantity of random boxes. For simplicity, assume all boxes fit in the
+        specified warehouse area.
       </Typography>
     </Stack>
   );
